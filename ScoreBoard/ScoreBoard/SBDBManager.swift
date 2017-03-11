721733bd0a894df47c67e43ac35e4086a2ecaa1b
Abhishek Thapliyal <Abhishek@Abhisheks-MacBook-Air.local>
2017-03-11 20:08:50 +0530

    #1

---
+++ ScoreBoard/ScoreBoard/SBDBManager.swift
@@ -0,0 +1,17 @@
+//
+//  SBDBManager.swift
+//  ScoreBoard
+//
+//  Created by Abhishek Thapliyal on 3/11/17.
+//  Copyright © 2017 Abhishek Thapliyal. All rights reserved.
+//
+
+import UIKit
+
+class SBDBManager: NSObject {
+
+    public func insertEntity(player: SBPlayer) {
+        
+    }
+    
+}
