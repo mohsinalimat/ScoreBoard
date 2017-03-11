054f73ea8c0f8c772c1e6a8d0bbff76c9394caf2
Abhishek Thapliyal <Abhishek@Abhisheks-MacBook-Air.local>
2017-03-11 21:19:58 +0530

    #1

--- ScoreBoard/ScoreBoard/SBDBManager.swift
+++ ScoreBoard/ScoreBoard/SBDBManager.swift
@@ -7,11 +7,51 @@
 //
 
 import UIKit
+import CoreData
 
 class SBDBManager: NSObject {
 
     public func insertEntity(player: SBPlayer) {
+     
+        let dbPersistence = SBDBPersistence.sharedInstance
+        let context = dbPersistence.persistentContainer.viewContext as NSManagedObjectContext
         
+        let entityDescription  = NSEntityDescription.entity(forEntityName: "Favourite", in: context)
+        
+        let favouriteObj = Favourite(entity: entityDescription!, insertInto: context)
+        
+        favouriteObj.id = player.id as String?
+        favouriteObj.favourite = player.favourite!
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    public func fetchEntity(id: NSString?) -> SBPlayer {
+        
+        let dbPersistence = SBDBPersistence.sharedInstance
+        let context = dbPersistence.persistentContainer.viewContext as NSManagedObjectContext
+        let predicate = NSPredicate(format:"id == %@", id!)
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Favourite")
+        fetchRequest.predicate = predicate
+        
+        let playerObj = SBPlayer()
+        do {
+            let resultArray = try context.fetch(fetchRequest) as! [NSManagedObject]
+            
+            if ((resultArray.count) > 0) {
+                let player = (resultArray[0])
+                playerObj.id = player.value(forKey: "id") as! NSString?
+                playerObj.favourite = (player.value(forKey: "favourite") as! Bool)
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        return playerObj
     }
     
 }
