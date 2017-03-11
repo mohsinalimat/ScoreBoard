a3fccc965e2b1c1f817f5cce8bc37b02522ee6b9
Abhishek Thapliyal <Abhishek@Abhisheks-MacBook-Air.local>
2017-03-11 20:07:15 +0530

    #1

---
+++ ScoreBoard/ScoreBoard/Favourite+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Favourite+CoreDataClass.swift
+//  ScoreBoard
+//
+//  Created by Abhishek Thapliyal on 3/11/17.
+//  Copyright © 2017 Abhishek Thapliyal. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Favourite)
+public class Favourite: NSManagedObject {
+
+}
---
+++ ScoreBoard/ScoreBoard/Favourite+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Favourite+CoreDataProperties.swift
+//  ScoreBoard
+//
+//  Created by Abhishek Thapliyal on 3/11/17.
+//  Copyright © 2017 Abhishek Thapliyal. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Favourite {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Favourite> {
+        return NSFetchRequest<Favourite>(entityName: "Favourite");
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var favourite: Bool
+
+}
