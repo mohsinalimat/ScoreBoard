7a3ebbbf6d4ce1e5ee79a8ac6a7ab50f34922845
Abhishek Thapliyal <Abhishek@Abhisheks-MacBook-Air.local>
2017-03-11 12:39:42 +0530

    #1

--- ScoreBoard/ScoreBoard/SBNetworkManager.swift
+++ ScoreBoard/ScoreBoard/SBNetworkManager.swift
@@ -14,73 +14,28 @@ import UIKit
 //}
 
 class SBNetworkManager: NSObject {
-
-    public var status : NSString?
-    public var source : NSString?
-    public var sortBy : NSString?
     
-    public var articles : NSArray?
-    public var sources : NSArray?
-    
-    public var articleArray : NSMutableArray?
-    public var sourceArray : NSMutableArray?
+     public var responseArray : NSMutableArray?
     
     //====================================================================================================================================
-    // GET ARTICLES METHOD
+    // GET PLAYERS METHOD
     //====================================================================================================================================
     
-    public func getNewsArticle(sortType:NSString, completion : @escaping (_ articleArray:NSArray, _ error:NSError?) -> Void) {
+    public func getPlayers(sortType:NSString, completion : @escaping (_ articleArray:NSArray, _ error:NSError?) -> Void) {
         
-        let URLString : String = "https://newsapi.org/v1/articles"
-        let param = String(format:"source=the-next-web&sortBy=latest&apiKey=%@", "")
+        let URLString : String = "http://hackerearth.0x10.info/api/gyanmatrix"
+        let param = "type=json&query=list_player"
         let request : NSMutableURLRequest = SBHTTPRequest.getServerRequest(urlString: URLString, paramString: param)
-        SBHTTPResponse.responseWithRequest(request: request, requestTitle: "FETCH_ARTICLES", completion: { (json, error) in
+        SBHTTPResponse.responseWithRequest(request: request, requestTitle: "FETCH_PLAYERS", completion: { (json, error) in
             
             print("ERROR :: \(error?.localizedDescription)")
-            self.articleArray = NSMutableArray()
+            self.responseArray = NSMutableArray()
             if (error == nil)
             {
                 let dictionary : [String:Any] = json as! [String : Any]
                 
-                self.status = dictionary["status"]! as? NSString
-                self.source = dictionary["source"]! as? NSString
-                self.sortBy = dictionary["sortBy"]! as? NSString
-                
-                self.articles = NSArray(array: dictionary["articles"] as! NSArray)
-                
-                for articleDict in self.articles as! [[String:Any]] {
-                    
-                }
             }
-            completion(self.articleArray!, error)
-        })
-    }
-    
-    //====================================================================================================================================
-    // GET SOURCE METHOD
-    //====================================================================================================================================
-    
-    public func getNewsSource(completion : @escaping (_ sourceArray:NSArray, _ error:NSError?) -> Void) {
-        
-        let URLString : String = "https://newsapi.org/v1/sources"
-        let param = String(format:"language=%@", LANGUAGES.ENG.rawValue)
-        let request : NSMutableURLRequest = SBHTTPRequest.getServerRequest(urlString: URLString, paramString: param)
-        SBHTTPResponse.responseWithRequest(request: request, requestTitle: "FETCH_SOURCES", completion: { (json, error) in
-            
-            print("ERROR :: \(error?.localizedDescription)")
-            self.sourceArray = NSMutableArray()
-            if (error == nil)
-            {
-                let dictionary : [String:Any] = json as! [String : Any]
-                
-                self.status = dictionary["status"]! as? NSString
-                
-                self.sources = NSArray(array: dictionary["sources"] as! NSArray)
-                for sourcesDict in self.sources as! [[String:Any]] {
-               
-                }
-            }
-            completion(self.sourceArray!, error)
+            completion(self.responseArray!, error)
         })
     }
 }
